034a972a56cb09aee05cced989082ad09730331d
Victor Andreescu <victor.andreescu@gmail.com>
2018-02-09 03:35:26 +0200

    fix schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:work --daemon')->everyMinute()->withoutOverlapping();
+        $schedule->command('queue:work --tries=10')->everyMinute()->withoutOverlapping();
         // $schedule->command('inspire')
         //          ->hourly();
     }
